75d768d310aac21818e7f112ee9a97cd41e9e38f
ÖMER FARUK <MER FARUK@LAPTOP-78EGM24J>
2022-11-07 21:34:33 +0200

    Proje dosyası ekle.

---
+++ Hafta3odev/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta3odev
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Hafta3odev/Program.cs
@@ -0,0 +1,64 @@
+﻿using Hafta3odev;
+
+public class Program
+{
+    public static void Main()
+    {
+        ShoppingCart cart = new ShoppingCart();
+
+        cart.Add(new CartItem()
+        {
+            CartId = 1,
+            Product = new Product()
+            {
+                ProductId = 0,
+                ProductName = "Pro01",
+                Price = 50
+            },
+            Quantity = 5,
+        });
+
+        cart.Add(new CartItem()
+        {
+            CartId = 2,
+            Product = new Product()
+            {
+                ProductId = 1,
+                ProductName = "Pro02",
+                Price = 70
+            },
+            Quantity = 14,
+        });
+
+        cart.Add(new CartItem()
+        {
+            CartId = 3,
+            Product = new Product()
+            {
+                ProductId = 2,
+                ProductName = "Pro03",
+                Price = 100
+            },
+            Quantity = 19,
+        });
+
+        var list = cart.GetList();
+        foreach (var item in list)
+            Console.WriteLine($"{item.CartId} " +
+                $"{item.Product.ProductName} " +
+                $"{item.Product.Price} " +
+                $"{item.Quantity}");
+
+        Console.WriteLine(cart.GetBalance(0));
+
+        Console.WriteLine(cart.Remove(1).Product.ProductName);
+
+        list = cart.GetList();
+
+        foreach (var item in list)
+            Console.WriteLine($"{item.CartId}  " +
+                $"{item.Product.ProductName}  " +
+                $"{item.Product.Price}  " +
+                $"{item.Quantity}");
+    }
+}
